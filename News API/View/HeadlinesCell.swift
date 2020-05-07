28bd17f2606ec894a186019abf2d6abd0b8620ff
Cao Mai <caomai@rocketmail.com>
2020-05-06 23:25:02 -0700

    [remove] unused method

--- News API/View/HeadlinesCell.swift
+++ News API/View/HeadlinesCell.swift
@@ -20,12 +20,6 @@ class HeadlinesCell: UITableViewCell {
         // Initialization code
     }
     
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-        // Configure the view for the selected state
-    }
-    
-    
     func setHeadlines(for article: Article) {
         
         headlineLabel.translatesAutoresizingMaskIntoConstraints = false
